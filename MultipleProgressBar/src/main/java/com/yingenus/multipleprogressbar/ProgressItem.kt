f15d392dbbb1f22498474e1d35be79c79df89647
yingenus <engeniyaton@gmail.com>
2021-09-21 08:58:41 +0300

    add label text

--- MultipleProgressBar/src/main/java/com/yingenus/multipleprogressbar/ProgressItem.kt
+++ MultipleProgressBar/src/main/java/com/yingenus/multipleprogressbar/ProgressItem.kt
@@ -7,17 +7,37 @@ import android.graphics.*
 import android.os.Build
 import android.os.Bundle
 import android.os.Parcelable
+import android.text.TextPaint
 import android.util.AttributeSet
 import android.view.View
 import androidx.annotation.ColorInt
 import androidx.annotation.ColorRes
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
 
 public class ProgressItem : View{
 
+    companion object{
+        const val ORIENTATION_RIGHT = 0
+        const val ORIENTATION_LEFT = 1
+        const val TEXT_GRAVITY_RIGHT = 2
+        const val TEXT_GRAVITY_LEFT = 1
+        const val TEXT_GRAVITY_ADAPTIVE = 0
+        const val TEXT_PERCENT = 0
+        const val TEXT_RAF = 1
+    }
+
     object Orientation{
         const val RIGHT = 0
         const val LEFT = 1
+    }
+
+    object TextGravity{
+        const val RIGHT = 2
+        const val LEFT = 1
+        const val ADAPTIVE = 0
     }
 
     private object SavedParams{
@@ -45,6 +65,36 @@ public class ProgressItem : View{
             field = value
             progress = progress
             secondaryProgress = secondaryProgress
+            requestInvalidate()
+        }
+    var labelText = ""
+        set(value) {
+            field = value
+            requestInvalidate()
+        }
+    var showLabel = false
+        set(value) {
+            field = value
+            requestInvalidate()
+        }
+    var labelGravity = TextGravity.ADAPTIVE
+        set(value) {
+            field = max(min(value,2),0)
+            requestInvalidate()
+        }
+    var progressText = TEXT_PERCENT
+        set(value) {
+            field = max(min(value,1),0)
+            requestInvalidate()
+        }
+    var showProgressText = false
+        set(value) {
+            field = value
+            requestLayout()
+        }
+    var progressTextGravity = TextGravity.ADAPTIVE
+        set(value) {
+            field = max(min(value,2),0)
             requestInvalidate()
         }
     private var progress: Int = 0
@@ -113,11 +163,33 @@ public class ProgressItem : View{
                 field = value
             }
         }
+    var progressTextColor = ColorStateList.valueOf(Color.BLACK)
+        set(value) {
+            if (field != value){
+                field = value
+                updateColors()
+            }else{
+                field = value
+            }
+        }
+    var labelColor = ColorStateList.valueOf(Color.BLACK)
+        set(value) {
+            if (field != value){
+                field = value
+                updateColors()
+            }else{
+                field = value
+            }
+        }
 
     private var currentProgressColor = Color.BLUE
     private var currentSecondaryProgressColor = Color.TRANSPARENT
+    private var currentProgressTextColor = Color.BLACK
+    private var currentLabelColor = Color.BLACK
 
     private lateinit var paint : Paint
+    private lateinit var labelPaint : Paint
+    private lateinit var textPaint : Paint
     private lateinit var rectF: RectF
     private var mBitmap : Bitmap? = null
     private var mCanvas : Canvas? = null
@@ -145,8 +217,20 @@ public class ProgressItem : View{
 
                 setProgress(attribute.getInteger(R.styleable.MultipleProgressBarItem_mpb_progress, progress), false)
                 setSecondaryProgress(attribute.getInteger(R.styleable.MultipleProgressBarItem_mpb_secondaryProgress,secondaryProgress), false)
+
                 progressColor = attribute.getStateList(R.styleable.MultipleProgressBarItem_mpb_progressColor,progressColor)
                 secondaryProgressColor = attribute.getStateList(R.styleable.MultipleProgressBarItem_mpb_secondaryProgressColor,secondaryProgressColor)
+
+                labelText = attribute.getString(R.styleable.MultipleProgressBarItem_mpb_labelText)?:labelText
+                showLabel = attribute.getBoolean(R.styleable.MultipleProgressBarItem_mpb_showLabel, false)
+                labelGravity = attribute.getInteger(R.styleable.MultipleProgressBarItem_mpb_labelGravity, labelGravity)
+                labelColor = attribute.getStateList(R.styleable.MultipleProgressBarItem_mpb_labelColor,labelColor)
+
+                progressText = attribute.getInteger(R.styleable.MultipleProgressBarItem_mpb_progressText,progressText)
+                progressColor = attribute.getStateList(R.styleable.MultipleProgressBarItem_mpb_progressColor,progressColor)
+                showProgressText = attribute.getBoolean(R.styleable.MultipleProgressBarItem_mpb_showProgressText,false)
+                progressTextGravity = attribute.getInteger(R.styleable.MultipleProgressBarItem_mpb_ProgressTextGravity,progressTextGravity)
+
                 orientation = attribute.getInteger(R.styleable.MultipleProgressBarItem_mpb_orientation, orientation)
             }finally {
                 attribute.recycle()
@@ -230,6 +314,7 @@ public class ProgressItem : View{
 
         canvas.drawColor(0x00000000,PorterDuff.Mode.CLEAR)
 
+
         //draw secondaryProgress
         val secondaryPath = Path()
         secondaryPath.addArc(rectF, applyCorrection(getStartAngle().toFloat()),getExtraSecondProcessAngle().toFloat())
@@ -238,15 +323,166 @@ public class ProgressItem : View{
         canvas.drawPath(secondaryPath,paint)
 
         //draw progress
-
         val progressPath = Path()
         progressPath.addArc(rectF, applyCorrection(getStartAngle().toFloat()),getExtraProcessAngle().toFloat())
 
         paint.color = currentProgressColor
         canvas.drawPath(progressPath,paint)
 
+        //draw progress text
+        if (showProgressText){
+            drawProgressText(canvas)
+        }
+
+        //draw label
+        if (showLabel){
+            drawLabelText(canvas)
+        }
+
         canvas.restore()
     }
+
+    private fun drawLabelText(canvas: Canvas){
+        val start = getStartAngle().toFloat()
+        val end = max(getExtraSecondProcessAngle(), getExtraProcessAngle()).toFloat()
+
+        val bound : Rect = Rect()
+        labelPaint.getTextBounds(labelText, 0, labelText.length, bound)
+
+        val halfStroke = strokeWide/2
+        val stOffset = halfStroke - bound.height()/2
+        val counterOffset = strokeWide - stOffset
+
+        val diameter = width - counterOffset*2
+        val circleLength = diameter * Math.PI
+
+        val leftFree = circleLength * ((360 - end) / 360)
+        val rightFree = circleLength * (end / 360)
+
+
+        val requireWight = bound.width()
+
+        val gravity = if (labelGravity == TEXT_GRAVITY_ADAPTIVE) {
+            when {
+                rightFree > requireWight -> {
+                    TEXT_GRAVITY_RIGHT
+                }
+                leftFree > requireWight -> {
+                    TEXT_GRAVITY_LEFT
+                }
+                else -> labelGravity
+            }
+        } else {
+            labelGravity}
+
+        val startAng : Float
+        val endAng : Float
+        val roundingOffset : Float
+
+        when {
+            requireWight == 0 -> {
+                startAng = 0f
+                endAng = 0f
+                roundingOffset = 0f
+            }
+            gravity == TEXT_GRAVITY_LEFT && leftFree > requireWight -> {
+                startAng = 360 - ((360 * requireWight) / circleLength).toFloat()
+                endAng = 360f
+                roundingOffset = ((360 * strokeWide.toFloat()/2) / circleLength).toFloat()
+            }
+            gravity == TEXT_GRAVITY_RIGHT && rightFree > requireWight -> {
+                startAng = 0f
+                endAng = ((360 * requireWight) / circleLength).toFloat()
+                roundingOffset = 0f
+            }
+            else -> {
+                startAng = 0f
+                endAng = 0f
+                roundingOffset = 0f
+            }
+        }
+
+
+
+        //val halfStroke = strokeWide/2
+        //val stOffset = halfStroke - bound.height()/2
+        //val counterOffset = strokeWide - stOffset
+
+        val rect = RectF(counterOffset,counterOffset,width - counterOffset,height - counterOffset)
+        val counter = Path()
+
+        counter.addArc(rect, applyCorrection(startAng-roundingOffset), applyCorrection(endAng-roundingOffset))
+
+        labelPaint.color = currentLabelColor
+
+        canvas.drawTextOnPath(labelText.toCharArray(), 0, labelText.length, counter, 0f,0f,labelPaint)
+    }
+
+    private fun drawProgressText(canvas: Canvas){
+        val start = getStartAngle()
+        val end = getExtraProcessAngle()
+
+        val diameter = width - strokeWide
+        val circleLength = diameter * Math.PI
+
+        val leftFree = circleLength * (abs(end - start) / 360)
+        val rightFree = circleLength * (abs(start - end) / 360)
+
+        val tempText = "100"
+
+        val bound : Rect = Rect()
+        textPaint.getTextBounds(tempText, 0, tempText.length, bound)
+
+        val requireWight = bound.width()
+
+        val gravity = if (labelGravity == TEXT_GRAVITY_ADAPTIVE) {
+            when {
+                rightFree > requireWight -> {
+                    TEXT_GRAVITY_RIGHT
+                }
+                leftFree > requireWight -> {
+                    TEXT_GRAVITY_LEFT
+                }
+                else -> labelGravity
+            }
+        } else {
+            labelGravity}
+
+        val startAng : Int
+        val endAng : Int
+
+        when {
+            requireWight == 0 -> {
+                startAng = 0
+                endAng = 0
+            }
+            labelGravity == TEXT_GRAVITY_LEFT && leftFree > requireWight -> {
+                startAng = 360 - ((360 * requireWight) / circleLength) as Int
+                endAng = 360
+            }
+            labelGravity == TEXT_GRAVITY_RIGHT && rightFree > requireWight -> {
+                startAng = 0
+                endAng = ((360 * requireWight) / circleLength) as Int
+            }
+            else -> {
+                startAng = 0
+                endAng = 0
+            }
+        }
+
+
+        val halfStroke = strokeWide/2
+        val rect = RectF(0f + halfStroke,0f + halfStroke,width - halfStroke,height - halfStroke)
+        val counter = Path()
+
+        counter.addArc(rect, startAng.toFloat(), endAng.toFloat())
+
+        textPaint.color = currentProgressTextColor
+
+        canvas.drawTextOnPath(tempText.toCharArray(), 0, tempText.length, counter, 0f,0f,textPaint)
+    }
+
+
 
     override fun drawableStateChanged() {
         super.drawableStateChanged()
@@ -260,16 +496,33 @@ public class ProgressItem : View{
         paint.strokeCap = Paint.Cap.ROUND
         paint.strokeWidth = strokeWide
         paint.isAntiAlias = true
+        labelPaint = TextPaint()
+        labelPaint.textSize = strokeWide
+        labelPaint.isAntiAlias = true
+        labelPaint.isSubpixelText = true
+        textPaint = TextPaint()
+        textPaint.textSize = strokeWide
+        textPaint.isAntiAlias = true
+        textPaint.isSubpixelText = true
+
     }
 
     private fun updateColors(){
         val newProcessColor = progressColor.getColorForState(drawableState,Color.TRANSPARENT)
         val newSecondaryProgressColor = secondaryProgressColor.getColorForState(drawableState,Color.TRANSPARENT)
+        val newProgressTextColor = progressTextColor.getColorForState(drawableState,Color.TRANSPARENT)
+        val newLabelColor = labelColor.getColorForState(drawableState,Color.TRANSPARENT)
 
-        if (newProcessColor != currentProgressColor || newSecondaryProgressColor != currentSecondaryProgressColor)
+        if (newProcessColor != currentProgressColor ||
+                newSecondaryProgressColor != currentSecondaryProgressColor ||
+                newProgressTextColor != currentProgressTextColor ||
+                newLabelColor != currentLabelColor) {
             smtChanged = true
+        }
 
         currentProgressColor = newProcessColor
+        currentProgressTextColor = newProgressTextColor
+        currentLabelColor = newLabelColor
         currentSecondaryProgressColor = newSecondaryProgressColor
     }
 
